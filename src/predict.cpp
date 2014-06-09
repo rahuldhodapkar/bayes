94ce67f7f8e60d6476ad06efed3f7bef5f23f4b1
Rahul Dhodapkar <rahuldhodapkar@tests-MacBook-Pro-6.local>
2014-06-09 11:02:45 -0400

    adding scaffolding code for MCMC prediction

---
+++ src/predict.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* ==== predict.cpp ====
+ * this is the main module of a small C++ program written to implement
+ * MCMC-based heirarchical modeling and get a better handle on the 
+ * problem before attempting to implement it in the MongoDB kernel
+ *
+ * @author Rahul Dhodapkar (krishnakid)
+ * @version 6.9.2014
+ */
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
