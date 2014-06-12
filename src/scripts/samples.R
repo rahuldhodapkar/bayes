29f143fa14e999f398fd5c81c374b7f4a2028a93
krishnakid <rahul.m.dhodapkar@gmail.com>
2014-06-12 17:40:57 -0400

    changed scripts to stash truth values, as well as to proplerly draw overlaying histograms

--- src/scripts/samples.R
+++ src/scripts/samples.R
@@ -7,38 +7,30 @@
 # @author Rahul Dhodapkar (krishnakid)
 # @version 6.10.2014
 
-
 # build a ground truth histogram
 
-nSamples <- 5000;
-nBins <- 100;
+nSamples <- 10000;
+low <- 0;
+high <- 100;
 
-vals = rnorm(nSamples, mean=50, sd=10);
+vals = rnorm(nSamples, mean=50, sd=20);
 # vals = rbinom(nSamples, size=10, prob=0.2);
 
-vals = floor(vals);
-vals = vals[vals > 0];
-vals = vals[vals <= nBins];
-truth = matrix(0, 1, nBins);
+lbnd <- runif(nSamples, min=low, max=high);
+rbnd <- runif(nSamples, min=low, max=high);
 
-for (val in vals) {
-    truth[val] <- truth[val] + 1;
-}
-
-lbnd <- sample(1:nBins, nSamples, replace=TRUE); # can be extended to runif
-rbnd <- sample(1:nBins, nSamples, replace=TRUE);
-
-ranges <- matrix(, nSamples, 3); 
+ranges <- matrix(, nSamples, 3);
 
 for (i in 1:nSamples) {
-    ranges[i,1] <- min(lbnd[i], rbnd[i]);
-    ranges[i,2] <- max(lbnd[i], rbnd[i]);
-    ranges[i,3] <- sum(truth[ranges[i,1]:ranges[i,2]]);
+    ranges[i,1] <- min(lbnd[i], rbnd[i]);    
+    ranges[i,2] <- max(lbnd[i], rbnd[i]);    
+
+    ranges[i,3] <- length(vals[vals > ranges[i,1] & vals < ranges[i,2]]);
 }
-# ranges now complete, print to file.
 
 write.table(ranges, "../../data/gen.in", sep=",", 
             row.names=FALSE, col.names=FALSE);
 
-print("recall TRUTH:");
-print(truth);
+write.table(vals, "../../data/gen.truth", sep=",",
+            row.names=FALSE, col.names=FALSE);
+
--- src/scripts/visualize.R
+++ src/scripts/visualize.R
@@ -14,19 +14,40 @@ library("ggplot2");         # load ggplot2 graphics library
 x <- numeric();
 y <- numeric();
 
+xt <- numeric();
+yt <- numeric();
+
 raw = read.csv("../../out/hist.out", header=FALSE);
+truth = read.csv("../../data/gen.truth", header=FALSE);
 
 sts = raw[,1];
 stps = raw[,2];
 vals = raw[,3]/(raw[,2] - raw[,1]);
+
+tvals <- numeric();
+for (i in 1:length(vals))
+{
+    tvals = c(tvals, length(truth[truth > sts[i] & truth < stps[i]]));
+}
+tvals = tvals/(raw[,2] - raw[,1]);
 
 for (i in 1:length(vals))
 {
     x = c(x, sts[i]);
     y = c(y, vals[i]);
 
+    xt = c(xt, sts[i]);
+    yt = c(yt, tvals[i]);
+
+
     x = c(x, stps[i]);
     y = c(y, vals[i]);
+
+    xt = c(xt, stps[i]);
+    yt = c(yt, tvals[i]);
+
 }
 
-plot(x, y, type="l");
+plot(xt,yt,type="l");
+lines(x,y,col="blue");
+
