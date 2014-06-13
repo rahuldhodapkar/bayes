b8cb2cd86a2bd654a839de5e0ee00e4e70a44969
krishnakid <rahul.m.dhodapkar@gmail.com>
2014-06-13 15:36:10 -0400

    cleaned up R scripts and got rid of div/0 error

--- src/scripts/samples.R
+++ src/scripts/samples.R
@@ -13,8 +13,20 @@ nSamples <- 10000;
 low <- 0;
 high <- 100;
 
-vals = rnorm(nSamples, mean=50, sd=20);
-# vals = rbinom(nSamples, size=10, prob=0.2);
+# vals = rnorm(nSamples, mean=50, sd=20);        # normal
+# vals = rcauchy(nSamples, location = 50, scale=9);     # cauchy
+
+# vals = rbinom(nSamples, size=25, prob=0.2);    # binomial
+
+# vals = sample(c(40, 40, 40), nSamples,
+#                         replace=TRUE);
+
+vals = sample(c(40, 40, 40, 24, 24, 24, 66.3, 66.3), nSamples,
+                         replace=TRUE);
+
+#vals = sample(c(40, 40, 40, 24, 24, 24, 66.3, 66.3, 90, 15), nSamples,
+#                        replace=TRUE);
+
 
 lbnd <- runif(nSamples, min=low, max=high);
 rbnd <- runif(nSamples, min=low, max=high);
@@ -28,9 +40,9 @@ for (i in 1:nSamples) {
     ranges[i,3] <- length(vals[vals > ranges[i,1] & vals < ranges[i,2]]);
 }
 
-write.table(ranges, "../../data/gen.in", sep=",", 
+write.table(ranges, "../data/gen.in", sep=",", 
             row.names=FALSE, col.names=FALSE);
 
-write.table(vals, "../../data/gen.truth", sep=",",
+write.table(vals, "../data/gen.truth", sep=",",
             row.names=FALSE, col.names=FALSE);
 
--- src/scripts/visualize.R
+++ src/scripts/visualize.R
@@ -7,8 +7,6 @@
 # @author Rahul Dhodapkar (krishnakid)
 # @version 6.12.14
 
-library("ggplot2");         # load ggplot2 graphics library
-
 # we will be drawing the histogram as a line plot for maximum effect.
 
 x <- numeric();
@@ -17,8 +15,8 @@ y <- numeric();
 xt <- numeric();
 yt <- numeric();
 
-raw = read.csv("../../out/hist.out", header=FALSE);
-truth = read.csv("../../data/gen.truth", header=FALSE);
+raw = read.csv("../out/hist.out", header=FALSE);
+truth = read.csv("../data/gen.truth", header=FALSE);
 
 sts = raw[,1];
 stps = raw[,2];
@@ -36,18 +34,32 @@ for (i in 1:length(vals))
     x = c(x, sts[i]);
     y = c(y, vals[i]);
 
-    xt = c(xt, sts[i]);
-    yt = c(yt, tvals[i]);
-
-
     x = c(x, stps[i]);
     y = c(y, vals[i]);
-
-    xt = c(xt, stps[i]);
-    yt = c(yt, tvals[i]);
-
 }
 
-plot(xt,yt,type="l");
+truthRes = 1;
+
+tvals <- numeric();
+for (i in 1:(100/truthRes))
+{
+    tvals = c(tvals, length(truth[truth >= (i-1)*truthRes &
+                                  truth < i*truthRes]));
+}
+
+tvals = tvals/truthRes;
+
+for (i in 1:length(tvals))
+{
+    xt = c(xt, truthRes*(i-1));
+    yt = c(yt, tvals[i]);
+
+    xt = c(xt, truthRes*i);
+    yt = c(yt, tvals[i]);
+}
+
+png("../figures/histfit.png");
+plot(xt,yt,type="l", xlab = "value", ylab = "# in range");
 lines(x,y,col="blue");
+dev.off();
 
